bddada39b0ab7ed236d4f389f0fe0c35c79ba954
ThePoopLord <88780282+ThePoopLord@users.noreply.github.com>
2022-01-02 16:17:31 +0200

    Update source_code.cpp

--- source_code.cpp
+++ source_code.cpp
@@ -8,13 +8,9 @@ int main() {
     cin >> limit;
     cout << "\n";
     for (int x = 1; x <= limit; x++) {
-        //cout << "x is equal to: " <<x <<"\n";
         for (int y = 2; y <= limit; y++) {
-            //cout <<"y is equal to: " <<y<< "\n";
             if (x != y){
                 if (x%y == 0) {
-                    //cout << x << "\n";
-                    //int y = 1;
                     break;
                 }
             }
